07ac50385ee98f896695cfc3fc31ce2d58ae77a9
Jeremiah <phase.variance@gmail.com>
2019-04-25 16:36:23 -0400

    Initial Version

---
+++ commit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Import settings from config file
+source /home/root/irule_export/config.sh
+#Export current irules from F5
+tmsh list ltm rule > /tmp/rules.txt
+#Encode irules to base64
+file_content=$(base64 -w0 /tmp/rules.txt)
+#Get the SHA256 hash of the current rules file from github
+json=$(curl -s --header "Authorization: token $token" $url)
+sha=$(grep -Po '"sha": "\K[a-z0-9]+' <<<$json)
+#Prepare data to be sent to github
+data='{"message":"Created via API","committer": {"name": "'$name'","email": "'$email'"},"content": "'$file_content'","sha": "'$sha'"}'
+#Commit updated irules to github
+#curl -i -X PUT --header "Authorization: token $token" --data "$data" $url
+curl -i -X PUT --header "Authorization: token $token" --data @- $url <<CURL_DATA
+$data
+CURL_DATA
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
---
+++ config.sh
@@ -0,0 +1,4 @@
+name="User Name"
+email="address@domain.com"
+token="token"
+url="https://api.github.com/repos/<owner>/<repo_name>/contents/<file>"
