487c7cb36fed40e3b69bc48e91cff0d70c322074
Jeremiah <phase.variance@gmail.com>
2019-04-25 20:07:50 -0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,6 +3,9 @@
 source /home/root/irule_export/config.sh
 #Export current irules from F5
 tmsh list ltm rule > /tmp/rules.txt
+#Check if file has changed since last run
+hash=`sha256sum /tmp/rules.txt`
+if [[ $(< /tmp/rules.sha) != $hash ]]; then
 #Encode irules to base64
 file_content=$(base64 -w0 /tmp/rules.txt)
 #Get the SHA256 hash of the current rules file from github
@@ -11,27 +14,8 @@ sha=$(grep -Po '"sha": "\K[a-z0-9]+' <<<$json)
 #Prepare data to be sent to github
 data='{"message":"Created via API","committer": {"name": "'$name'","email": "'$email'"},"content": "'$file_content'","sha": "'$sha'"}'
 #Commit updated irules to github
-#curl -i -X PUT --header "Authorization: token $token" --data "$data" $url
-curl -i -X PUT --header "Authorization: token $token" --data @- $url <<CURL_DATA
+curl -s -X PUT --header "Authorization: token $token" --data @- $url <<CURL_DATA
 $data
 CURL_DATA
-
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
+sha256sum /tmp/rules.txt > /tmp/rules.sha
+fi
